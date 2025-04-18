54be708eb8fe86ade081a808b5d96a051a3621b2
Georgy Charkseliani <giordano2407@gmail.com>
2025-04-18 12:47:47 +0200

    feat: add fif fucntion for rg and fzf

--- home/common.nix
+++ home/common.nix
@@ -53,6 +53,11 @@
       bindkey "^[[1;3C" forward-word
       bindkey "^[[1;3D" backward-word
 
+      fif() {
+        if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+        rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+      }
+
       # Environment variables
       export do="--dry-run=client -o yaml"
       export now="--force --grace-period 0"
