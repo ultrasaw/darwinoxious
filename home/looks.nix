5528c8642e486a681836b5d34dfaec08b8f3efe0
Georgy Charkseliani <giordano2407@gmail.com>
2025-04-30 13:54:36 +0200

    feat: add new layout zellij

--- home/looks.nix
+++ home/looks.nix
@@ -13,6 +13,12 @@
       ".config/starship.toml".source = ../dotfiles/.config/starship/starship.toml;
       # ".config/rstudio/themes".source = ../dotfiles/.config/rstudio/themes;
       ".config/zellij".source = ../dotfiles/.config/zellij;
+
+      # still have to do 'chmod +x dotfiles/.config/zellij/scripts/fzf-hx-open.sh'
+      ".config/zellij/scripts/fzf-hx-open.sh" = {
+        text      = builtins.readFile ../dotfiles/.config/zellij/scripts/fzf-hx-open.sh;
+        executable = true;
+      };
     };
   };
-} 
+}
