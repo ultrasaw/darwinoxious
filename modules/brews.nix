443c9187123e9448444cdfdf5cb4008b48528d4b
Georgy Charkseliani <giordano2407@gmail.com>
2026-08-19 12:31:40 +0200

    feat: add circleci

--- modules/brews.nix
+++ modules/brews.nix
@@ -3,6 +3,7 @@ _:
 [
   "glow" # render .md on the CLI
   "argo" # argo workflows
+  "circleci"
   "tree"
   "coreutils" # for 'timeout'
   "xcodes" # download/install full Xcode from Apple Developer
